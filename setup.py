48e79fb772e9075ad8475768dd285ca00f262451
ljwolf <levi.john.wolf@gmail.com>
2018-08-06 12:24:26 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import os.path
+
+from distutils.command.build_py import build_py
+
+from shapestats import __version__
+
+pth = os.path.dirname(os.path.abspath(__file__))+ '/requirements.txt'
+
+REQUIREMENTS = [i.strip() for i in open(pth).readlines()]
+
+
+setup(name='shapestats', # name of package
+      version=__version__,
+      description='tools & methods to measure shape regularity', #short <80chr description
+      url='https://github.com/ljwolf/shapestats', #github repo
+      maintainer='Levi John Wolf',
+      maintainer_email='levi.john.wolf@gmail.com',
+      tests_require=['pytest'],
+      keywords='spatial statistics',
+      classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+        ],
+      license='MIT',
+      packages=['shapestats'], # add your package name here as a string
+      install_requires=REQUIREMENTS,
+      zip_safe=False,
+      cmdclass = {'build.py':build_py})
