6afe7dd1355eefd54ff2adab05cbf798e38c56c4
ljwolf <levi.john.wolf@gmail.com>
2018-08-06 14:07:33 -0700

    update tests for shapestats

--- shapestats/tests/test_circles.py
+++ shapestats/tests/test_circles.py
@@ -3,14 +3,14 @@ from numpy import testing
 from ..minbc import minimum_bounding_circle
 from ..maxbc import maximum_contained_circle
 from ..compactness import _get_pointset
-from .test_measures import shape
+from .test_measures import shape, ATOL
 from shapely.geometry import Polygon
 
 pointset = _get_pointset(shape)
 
 def test_minbc():
     radius, center = minimum_bounding_circle(pointset) 
-    testing.assert_allclose(radius, .800390)
+    testing.assert_allclose(radius, .800390, atol=ATOL)
     testing.assert_array_equal((0.625, 0.5), center)
     circ = minimum_bounding_circle(shape)
     assert isinstance(circ, Polygon)
@@ -18,7 +18,7 @@ def test_minbc():
 
 def test_maxbc():
     radius, center = maximum_contained_circle(pointset)
-    testing.assert_allclose(radius, .309359)
+    testing.assert_allclose(radius, .309359, atol=ATOL)
     testing.assert_array_equal((0.4375, 0.5), center)
     circ = minimum_bounding_circle(shape)
     assert isinstance(circ, Polygon)
--- shapestats/tests/test_measures.py
+++ shapestats/tests/test_measures.py
@@ -11,53 +11,55 @@ shape = geometry.Polygon([(0,0),
                           (.75,.5),
                           (1.25,0)])
 
+ATOL = .001
+
 def test_boundary_amplitude():
     observed = boundary_amplitude(shape)
-    testing.assert_allclose(observed, .084452)
+    testing.assert_allclose(observed, .844527, atol=ATOL)
 
 def test_convex_hull():
     observed = convex_hull(shape)
-    testing.assert_allclose(observed, .7)
+    testing.assert_allclose(observed, .7, atol=ATOL)
 
 def test_eig_seitzinger():
     observed = eig_seitzinger(shape)
-    testing.assert_allclose(observed, .25)
+    testing.assert_allclose(observed, .25, atol=ATOL)
 
 def test_flaherty_crumplin():
     observed = flaherty_crumplin_lw(shape)
-    testing.assert_allclose(observed, .220863)
+    testing.assert_allclose(observed, .220863, atol=ATOL)
     observed = flaherty_crumplin_radius(shape)
-    testing.assert_allclose(observed, .659366)
+    testing.assert_allclose(observed, .659366, atol=ATOL)
 
 def test_iaq():
     observed = iaq(shape)
-    testing.assert_allclose(observed, .622314)
+    testing.assert_allclose(observed, .622314, atol=ATOL)
     observed2 = schwartzberg(shape)
-    testing.assert_allclose(observed, observed2)
+    testing.assert_allclose(observed, observed2, atol=ATOL)
 
 def test_ipq():
     observed = ipq(shape)
-    testing.assert_allclose(observed, .387275)
+    testing.assert_allclose(observed, .387275, atol=ATOL)
     observed2 = polsby_popper(shape)
-    testing.assert_allclose(observed, observed2)
+    testing.assert_allclose(observed, observed2, atol=ATOL)
 
 def test_moa():
     observed = moa_ratio(shape)
-    testing.assert_allclose(observed, 3.249799)
+    testing.assert_allclose(observed, 3.249799, atol=ATOL)
 
 def test_moment_of_interia():
     observed = moment_of_inertia(shape)
-    testing.assert_allclose(observed, .315715)
+    testing.assert_allclose(observed, .315715, atol=ATOL)
 
 def test_nmi():
     observed = nmi(shape)
-    testing.assert_allclose(observed, .487412)
+    testing.assert_allclose(observed, .487412, atol=ATOL)
 
 def test_reock():
     observed = reock(shape)
-    testing.assert_allclose(observed, .434764)
+    testing.assert_allclose(observed, .434764, atol=ATOL)
 
 def test_taylor():
     observed = taylor_reflexive(shape)
-    testing.assert_allclose(observed, -.33333)
+    testing.assert_allclose(observed, -.33333, atol=ATOL)
 
