894f48f7ff4cfde40eae8b06e5d305b1a9f064ac
ljwolf <levi.john.wolf@gmail.com>
2018-08-06 12:13:24 -0700

    move circle tests

---
+++ shapestats/tests/test_circles.py
@@ -0,0 +1,25 @@
+from shapely import geometry
+from numpy import testing
+from ..minbc import minimum_bounding_circle
+from ..maxbc import maximum_contained_circle
+from ..compactness import _get_pointset
+from .test_measures import shape
+from shapely.geometry import Polygon
+
+pointset = _get_pointset(shape)
+
+def test_minbc():
+    radius, center = minimum_bounding_circle(pointset) 
+    testing.assert_allclose(radius, .800390)
+    testing.assert_array_equal((0.625, 0.5), center)
+    circ = minimum_bounding_circle(shape)
+    assert isinstance(circ, Polygon)
+
+
+def test_maxbc():
+    radius, center = maximum_contained_circle(pointset)
+    testing.assert_allclose(radius, .309359)
+    testing.assert_array_equal((0.4375, 0.5), center)
+    circ = minimum_bounding_circle(shape)
+    assert isinstance(circ, Polygon)
+
